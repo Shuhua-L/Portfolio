f2295c40009f6556b776ac37b8c75b10d44af004
Shuhua-L <sl.shuhualiu@gmail.com>
2023-07-09 02:11:04 -0400

    🌈 style(header): extra div
    added a div to wrap the header content

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -16,48 +16,54 @@ const Header = () => {
 
   return (
     <header
-      className='bg-primary sticky w-full top-0 p-5 py-8 font-medium flex items-start justify-between
-        max-w-7xl mx-auto xl:items-center text-lg z-10
+      className='bg-primary sticky w-full top-0 p-5 py-8 font-medium text-lg z-10
         border-b-2 border-inherit dark:border-neutral-400'>
-      <nav className='hidden w-full md:block md:w-auto space-x-4'>
-        <NavLinks />
-      </nav>
+      <div
+        className='flex items-start justify-between
+        max-w-7xl mx-auto xl:items-center '>
+        <nav className='hidden w-full md:block md:w-auto space-x-4'>
+          <NavLinks />
+        </nav>
 
-      {/* Mobile Navbar */}
-      <button className='md:hidden' onClick={() => setOpenMenu(!openMenu)}>
-        <BarsIcon />
-      </button>
-      {openMenu && (
-        <div
-          className='z-10 h-screen w-full top-0 left-0 fixed bg-primary
-        flex items-center justify-center text-center'>
-          <span className='absolute top-8 right-8' onClick={() => setOpenMenu(!openMenu)}>
-            <CloseIcon className='w-8 h-8 cursor-pointer' />
-          </span>
-          <nav
-            className='flex flex-col gap-8 font-medium text-xl '
-            onClick={() => setOpenMenu(!openMenu)}>
-            <NavLinks />
-          </nav>
-        </div>
-      )}
-
-      <nav className='flex items-center justify-center flex-wrap'>
-        <Link href='https://github.com/Shuhua-L' target={"_blank"} className='w-6 mx-3'>
-          <GithubIcon />
-        </Link>
-        <Link href='https://www.linkedin.com/in/shuhua-liu/' target={"_blank"} className='w-6 mr-3'>
-          <LinkedinIcon />
-        </Link>
-        <button className='w-6 mr-3' onClick={toggleTheme}>
-          {mode === "dark" ? (
-            <MoonIcon />
-          ) : (
-            // <Image src='/icons8-sun.gif' alt='sun icon' width={32} height={32}></Image>
-            <SunIcon />
-          )}
+        {/* Mobile Navbar */}
+        <button className='md:hidden' onClick={() => setOpenMenu(!openMenu)}>
+          <BarsIcon />
         </button>
-      </nav>
+        {openMenu && (
+          <div
+            className='z-10 h-screen w-full top-0 left-0 fixed bg-primary
+        flex items-center justify-center text-center'>
+            <span className='absolute top-8 right-8' onClick={() => setOpenMenu(!openMenu)}>
+              <CloseIcon className='w-8 h-8 cursor-pointer' />
+            </span>
+            <nav
+              className='flex flex-col gap-8 font-medium text-xl '
+              onClick={() => setOpenMenu(!openMenu)}>
+              <NavLinks />
+            </nav>
+          </div>
+        )}
+
+        <nav className='flex items-center justify-center flex-wrap'>
+          <Link href='https://github.com/Shuhua-L' target={"_blank"} className='w-6 mx-3'>
+            <GithubIcon />
+          </Link>
+          <Link
+            href='https://www.linkedin.com/in/shuhua-liu/'
+            target={"_blank"}
+            className='w-6 mr-3'>
+            <LinkedinIcon />
+          </Link>
+          <button className='w-6 mr-3' onClick={toggleTheme}>
+            {mode === "dark" ? (
+              <MoonIcon />
+            ) : (
+              // <Image src='/icons8-sun.gif' alt='sun icon' width={32} height={32}></Image>
+              <SunIcon />
+            )}
+          </button>
+        </nav>
+      </div>
     </header>
   );
 };
