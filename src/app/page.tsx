9ba90a015fa4877d47fb599fd60eb4065217c978
Shuhua-L <sl.shuhualiu@gmail.com>
2023-07-11 02:13:51 -0400

    🌈 style(app): mobile view
    adjusted minor styles for mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ import Projects from "./sections/Projects";
 export default function Home() {
   return (
     <main
-      className='overflow-y-scroll #snap-proximity snap-y h-screen scroll-smooth #no-scrollbar
+      className='overflow-y-scroll snap-proximity snap-y h-screen scroll-smooth #no-scrollbar
     prose dark:prose-invert max-w-none prose-a:no-underline'>
       <Header />
-      <section id='home' className='snap-center flex flex-col relative w-full'>
+      <section id='home' className='snap-start flex flex-col relative w-full'>
         <Hero />
       </section>
       <section id='about' className='snap-start flex flex-col relative w-full'>
