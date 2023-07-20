12dc34f4b731d06e7f3afcea5273c760408141b8
Shuhua-L <sl.shuhualiu@gmail.com>
2023-07-19 23:08:55 -0400

    🌈 style(app): minor adjustments

--- src/app/components/Timeline/TimelineSection.tsx
+++ src/app/components/Timeline/TimelineSection.tsx
@@ -17,7 +17,11 @@ const TimelineSection = ({
       {direction === "left" || <TimelineBar position={position} />}
       <div
         className={`bg-secondary px-4 rounded-xl my-4 ml-auto shadow-md
-          ${direction === "left" ? "col-start-1 col-end-5" : "col-start-6 col-end-10"}`}>
+          ${
+            direction === "left"
+              ? "col-start-1 col-end-5 md:text-end"
+              : "col-start-6 col-end-10 md:text-start"
+          }`}>
         <h3 className=''>
           {title}
           <br />
